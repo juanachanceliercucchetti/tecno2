ad9b6016c25e71d43c0c2e9395656046e36ecd68
alexiaanduaga <129710474+alexiaand@users.noreply.github.com>
2024-05-15 23:05:06 -0300

    Update Imagenesn.js

--- Imagenesn.js
+++ Imagenesn.js
@@ -6,32 +6,24 @@ class Imagenesn {
     this.aln = aln;
     this.num = num;
     this.op = 255;
-    this.load();
-  }
-
-  load() {
-    this.imagenesn = [];
-    for (let i = 0; i < 12; i++) {
-      this.imagenesn[i] = loadImage("data/img" + i + ".png");
-    }
   }
 
   dibujar() {
     tint(255, this.op);
-    image(this.imagenesn[this.num], this.xn, this.yn, this.ann, this.aln);
+    image(imagenesn[this.num], this.xn, this.yn, this.ann, this.aln);
     noTint();
   }
 
   teclapresionada() {
     if (keyIsDown(RIGHT_ARROW)) {
       this.num++;
-      this.num %= this.imagenesn.length;
+      this.num %= imagenesn.length;
     }
     
     if (keyIsDown(LEFT_ARROW)) {
       this.num--;
       if (this.num < 0) {
-        this.num = 0;
+        this.num = imagenesn.length - 1;
       }
     }
     
