f7d5c63f5a881e6e6723cb7253abdb6f06d0b8ec
simongomezz <simgomez2005@gmail.com>
2024-05-18 00:57:09 -0300

    anterior

--- Imagenesn.js
+++ Imagenesn.js
@@ -5,26 +5,36 @@ class Imagenesn {
     this.ann = ann;
     this.aln = aln;
     this.num = num;
+    this.numAnterior = num;
     this.op = 255;
+    this.opCamibio = 1;
   }
 
   dibujar() {
-    tint(255, this.op);
+    this.opCamibio = lerp(this.opCamibio,1,0.1);
+
+    tint(255, this.op*this.opCamibio);
     image(imagenesn[this.num], this.xn, this.yn, this.ann, this.aln);
+    tint(255, this.op*(1-this.opCamibio));
+    image(imagenesn[this.numAnterior], this.xn, this.yn, this.ann, this.aln);
     noTint();
   }
 
   teclapresionada() {
     if (keyIsDown(RIGHT_ARROW)) {
+      this.numAnterior = this.num;
       this.num++;
       this.num %= imagenesn.length;
+      this.opCamibio = 0;
     }
     
     if (keyIsDown(LEFT_ARROW)) {
+      this.numAnterior = this.num;
       this.num--;
       if (this.num < 0) {
         this.num = imagenesn.length - 1;
       }
+      this.opCamibio = 0;
     }
     
     if (keyIsDown(68)) {
